0654e70f8f30d879039edfb23f88e4b69e47ac39
dataisinteresting <48961863+dataisinteresting@users.noreply.github.com>
2022-05-16 16:33:37 -0500

    Updated weight.py and weight.png

--- Weight.py
+++ Weight.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Creates an array of elapsed days and assigns it to days_elapsed
-days_elapsed = np.array([1, 2, 3 ,4, 5, 6, 7, 8, 9, 10,11]).reshape(-1,1)
-weight = np.array([163.2, 162.6, 163.4, 161.6, 161.8, 161.2, 160.0, 160.4, 160.0, 159.6,160.0])
+days_elapsed = np.array([1, 2, 3 ,4, 5, 6, 7, 8, 9, 10,11,12]).reshape(-1,1)
+weight = np.array([163.2, 162.6, 163.4, 161.6, 161.8, 161.2, 160.0, 160.4, 160.0, 159.6,160.0,159.2])
 
 # Instantiates a LinearRegression model and fits it to the above arrays
 model = sklearn.linear_model.LinearRegression().fit(days_elapsed,weight)
